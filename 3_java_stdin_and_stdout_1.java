1ff9ae0d01101d3f86b141c3d4bb1aea58bce1be
Shruti Srivastava <shrutishreya222@gmail.com>
2022-09-18 19:10:47 +0530

    problem 3

---
+++ 3_java_stdin_and_stdout_1.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner sc = new Scanner(System.in);
+        int i = sc.nextInt();
+        int j = sc.nextInt();
+        int k = sc.nextInt();
+        sc.close();
+        System.out.println(i);
+        System.out.println(j);
+        System.out.println(k);
+    }
+}
